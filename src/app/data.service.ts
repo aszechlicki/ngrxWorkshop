c7365648b23525c822e6f9eb0c483ad496e7f243
Arkadiusz Szechlicki <aszechlicki@sii.pl>
2020-02-20 15:33:11 +0100

    add presentation

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,59 +1,59 @@
-import { Injectable } from '@angular/core';
-import { Address } from './address';
-import { delay, filter, mergeMap, scan, shareReplay, tap } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
-import { Observable } from 'rxjs/internal/Observable';
-import { timer } from 'rxjs/internal/observable/timer';
-import { throwError } from 'rxjs/internal/observable/throwError';
-import { of } from 'rxjs/internal/observable/of';
+import { Injectable } from "@angular/core";
+import { Address } from "./address";
+import { delay, mergeMap, tap } from "rxjs/operators";
+import { BehaviorSubject } from "rxjs";
+import { Observable } from "rxjs/internal/Observable";
+import { timer } from "rxjs/internal/observable/timer";
+import { throwError } from "rxjs/internal/observable/throwError";
+import { of } from "rxjs/internal/observable/of";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
   private counter = 1;
-  private addressesSubject = new BehaviorSubject<Address[]>([{
-    company: 'Sii',
-    firstName: 'Arkadiusz',
-    lastName: 'Szechlicki',
-    address: 'Plac Kościeleckich 3',
-    address2: null,
-    city: 'Bydgoszcz',
-    state: 'AS',
-    postalCode: '12345',
-    shipping: 'free'
-  }]);
+
+  private addressesSubject = new BehaviorSubject<Address[]>([]);
   addressesList: Observable<Address[]> = this.addressesSubject.asObservable();
 
-  constructor() {
-  }
+  constructor() {}
 
   addAddress(item: Address) {
-    if (item.company !== 'Sii') {
+    if (item.company !== "Sii") {
       return timer(10000).pipe(
-        mergeMap(() => throwError('Company is not Sii!'))
+        mergeMap(() => throwError("Company is not Sii!"))
       );
     }
 
-    return of(item).pipe(delay(1000), tap(() => this.addressesSubject.next([...this.addressesSubject.value, item])));
+    return of(item).pipe(
+      delay(1000),
+      tap(() =>
+        this.addressesSubject.next([...this.addressesSubject.value, item])
+      )
+    );
   }
 
   loadAddresses() {
     if (this.counter++ % 5 === 0) {
-      return timer(10000).pipe(
-        mergeMap(() => throwError('Api error'))
-      );
+      return timer(10000).pipe(mergeMap(() => throwError("Api error")));
     }
     return this.addressesList.pipe(delay(500));
   }
 
   removeAddress(item: Address) {
-    if (item.city === 'Bydgoszcz') {
+    if (item.city === "Bydgoszcz") {
       return timer(10000).pipe(
-        mergeMap(() => throwError('We cant leave Bydgoszcz!'))
+        mergeMap(() => throwError("We can't leave Bydgoszcz!"))
       );
     }
 
-    return of(item).pipe(delay(1000), tap(() => this.addressesSubject.next(this.addressesSubject.value.filter(it => it !== item))));
+    return of(item).pipe(
+      delay(1000),
+      tap(() =>
+        this.addressesSubject.next(
+          this.addressesSubject.value.filter(it => it !== item)
+        )
+      )
+    );
   }
 }
--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-import { Observable } from 'rxjs/internal/Observable';
-import { Address } from '../address';
-import { EMPTY } from 'rxjs/internal/observable/empty';
+import { Component } from "@angular/core";
+import { DataService } from "../data.service";
+import { Observable } from "rxjs/internal/Observable";
+import { Address } from "../address";
+import { EMPTY } from "rxjs/internal/observable/empty";
 
 @Component({
-  selector: 'app-observables',
-  templateUrl: './observables.component.html',
-  styleUrls: ['./observables.component.scss']
+  selector: "app-observables",
+  templateUrl: "./observables.component.html",
+  styleUrls: ["./observables.component.scss"]
 })
-export class ObservablesComponent implements OnInit {
+export class ObservablesComponent {
   items: Observable<Address[]> = EMPTY;
 
-  constructor(private dataService: DataService) {
-  }
-
-  ngOnInit(): void {
-  }
+  constructor(private dataService: DataService) {}
 
   load() {
     this.items = this.dataService.loadAddresses();
